284bf5f219cb9bfe9a0f44a5c1c87824c3160d9e
DevGajjar12 <devkgajjar1271@gmail.com>
2024-06-02 15:52:30 -0400

    Error Solved for Card will close and open when toggled.

--- app/src/main/java/com/example/quickmart/CheckoutActivity.kt
+++ app/src/main/java/com/example/quickmart/CheckoutActivity.kt
@@ -1,20 +1,44 @@
 package com.example.quickmart
 
+
+
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class CheckoutActivity : AppCompatActivity() {
+    private lateinit var rgPaymentMode: RadioGroup
+    private lateinit var layoutCard: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_checkout)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        rgPaymentMode = findViewById(R.id.rgPaymentMode)
+        layoutCard = findViewById(R.id.layoutCard)
+
+        rgPaymentMode.setOnCheckedChangeListener { _, checkedId ->
+            when (checkedId) {
+                R.id.rbPOD -> {
+                    layoutCard.visibility = View.GONE
+                    Toast.makeText(this, "Selected: Pay on Delivery", Toast.LENGTH_SHORT).show()
+                    Log.d("CheckoutActivity", "Pay on Delivery selected")
+                }
+                R.id.rbDebit -> {
+                    layoutCard.visibility = View.VISIBLE
+                    Toast.makeText(this, "Selected: Credit or Debit", Toast.LENGTH_SHORT).show()
+                    Log.d("CheckoutActivity", "Credit or Debit selected")
+                }
+            }
+        }
+        if (rgPaymentMode.checkedRadioButtonId == R.id.rbDebit) {
+            layoutCard.visibility = View.VISIBLE
+        } else {
+            layoutCard.visibility = View.GONE
         }
     }
 }
